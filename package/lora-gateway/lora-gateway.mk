8acd629fa20753f95edc739516f88e53fab06f85
Darren Wenn <darren.wenn@microchip.com>
2019-01-17 15:42:49 +0000

    Added lora-gateway package and config files

---
+++ package/lora-gateway/lora-gateway.mk
@@ -0,0 +1,42 @@
+################################################################################
+#
+# lora-gateway
+#
+################################################################################
+
+LORA_GATEWAY_VERSION = v5.0.1
+LORA_GATEWAY_SITE = $(call github,Lora-net,lora_gateway,$(LORA_GATEWAY_VERSION))
+LORA_GATEWAY_LICENSE = Semtech S.A. 
+LORA_GATEWAY_LICENSE_FILES = LICENSE
+LORA_GATEWAY_INSTALL_STAGING = YES
+
+define LORA_GATEWAY_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) all
+endef
+
+define LORA_GATEWAY_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/libloragw/libloragw.a $(STAGING_DIR)/usr/lib/lora_gateway/libloragw/libloragw.a
+
+	$(foreach f,$(notdir $(wildcard $(@D)/libloragw/inc/*)),
+    	$(INSTALL) -m 0644 -D $(@D)/libloragw/inc/$(f) $(STAGING_DIR)/usr/include/lora_gateway/libloragw/inc)
+endef
+
+ifeq ($(BR2_PACKAGE_LORA_GATEWAY), y)
+define LORA_GATEWAY_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/reset_lgw.sh $(TARGET_DIR)/opt/lora_gateway/libloragw/reset_lgw.sh
+	( \
+		cd $(LORA_GATEWAY_BUILDDIR) ; \
+		for filename in $$(find ./ -name "util_*" -perm 0755 -type f); do \
+		$(INSTALL) -m 0755 -D $$filename $(TARGET_DIR)/opt/lora_gateway/utils/`basename $$filename`; \
+		done ; \
+	)
+endef
+endif
+
+$(eval $(generic-package))
+
+
+#		$(foreach filename, $(LORA_GATEWAY_UTIL_LIST), \
+#			find ./ -name $(filename) -perm 0755 -type f ; \
+#			) \
+
